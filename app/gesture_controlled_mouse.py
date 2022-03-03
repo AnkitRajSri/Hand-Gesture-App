89a24327f3d25c06ce838316ef6fc69245065ee3
Ankit Raj <ankitraj@Ankits-MacBook-Pro.local>
2022-03-02 23:31:45 -0700

    Fixed issues in gesture_controlled_mouse.py

--- app/gesture_controlled_mouse.py
+++ app/gesture_controlled_mouse.py
@@ -1,7 +1,8 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-import autopy
+import pyautogui
+
 
 # Object to store live video feed captured by the default device
 video = cv2.VideoCapture(0)
@@ -14,17 +15,24 @@ hands = mp_hand.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8)
 mp_draw = mp.solutions.drawing_utils
 
 # Store the width and height of the screen
-width_screen, height_screen = autopy.screen.size()
+width_screen, height_screen = pyautogui.size()
 
 prev_x, prev_y = 0, 0
 cur_x, cur_y = 0, 0
 
 
-def hand_landmarks(col_img):
+def hand_landmarks(img):
     landmark_list = []
 
+    # Convert the frames into an RGB image
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img.flags.writeable = False
+
     # Process the video frames using the mediapipe hands object
-    lm_positions = hands.process(col_img)
+    lm_positions = hands.process(img)
+
+    img.flags.writeable = True
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
     # Track the landmarks
     lm_track = lm_positions.multi_hand_landmarks
@@ -32,12 +40,15 @@ def hand_landmarks(col_img):
     if lm_track:
         for hand in lm_track:
             for idx, lm in enumerate(hand.landmark):
-                mp_draw.draw_landmarks(img, hand, mp_hand.HAND_CONNECTIONS)
-                h, w, c = img.shape
+                mp_draw.draw_landmarks(img, hand, mp_hand.HAND_CONNECTIONS,
+                                       mp_draw.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                       mp_draw.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2)
+                                       )
+                h, w, _ = img.shape
                 center_x, center_y = int(lm.x * w), int(lm.y * h)
                 landmark_list.append([idx, center_x, center_y])
 
-    return landmark_list
+    return img, landmark_list
 
 
 def finger_movements(landmarks):
@@ -53,9 +64,10 @@ def finger_movements(landmarks):
     else:
         finger_tips.append(0)
 
+    print(landmarks)
     # Check if the other fingers are up
     for i in range(1, 5):
-        if landmarks[tip_ids[i]][2] < landmarks[tip_ids[0]-3][2]:
+        if landmarks[tip_ids[i]][2] < landmarks[tip_ids[i]-3][2]:
             finger_tips.append(1)
         else:
             finger_tips.append(0)
@@ -65,12 +77,9 @@ def finger_movements(landmarks):
 
 while video.isOpened():
     # Read frames from the video captured
-    _, img = video.read()
+    _, frames = video.read()
 
-    # Convert the frames into an RGB image
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    lm_list = hand_landmarks(img_rgb)
-    # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    image, lm_list = hand_landmarks(frames)
 
     if len(lm_list) != 0:
         x_1, y_1 = lm_list[8][1:]
@@ -84,16 +93,16 @@ while video.isOpened():
             cur_x = prev_x + (x_3 - prev_x) / 7
             cur_y = prev_y + (y_3 - prev_y) / 7
 
-            autopy.mouse.move(cur_x, cur_y)
+            pyautogui.moveTo(width_screen - cur_x, cur_y)
             prev_x, prev_y = cur_x, cur_y
 
         if finger_tips[0] == 1 and finger_tips[1] == 0:
-            autopy.mouse.click()
+            pyautogui.click()
         print(finger_tips)
 
-    cv2.imshow("Hand Gesture", img)
+    cv2.imshow("Hand Gesture", cv2.flip(image, 1))
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# video.release()
-# cv2.destroyAllWindows()
+video.release()
+cv2.destroyAllWindows()
